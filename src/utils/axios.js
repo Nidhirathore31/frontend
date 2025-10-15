b580734e07edf0abfb1cf7e5e714ef40b2152b53
Nidhirathore31 <nidhirathore402@gmail.com>
2025-10-15 11:37:24 +0530

    url fixes

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL : "http://localhost:3001/"
-  baseURL : "https://backend-wsnj.onrender.com"
+  baseURL : "https://backend-wsnj.onrender.com/"
 });
 
 axiosInstance.interceptors.request.use((config)=>{
