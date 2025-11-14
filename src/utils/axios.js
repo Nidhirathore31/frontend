6d34355b9dcac72d6ec76680275375a8bab1c7f7
Nidhirathore31 <nidhirathore402@gmail.com>
2025-11-14 14:08:43 +0530

    fixed url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,38 @@
+// import store from "@/app/redux/store";
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   // baseURL : "http://localhost:3001/"  // for local development
+//   baseURL : "https://ecommerce-27cm.onrender.com/"  // ✅ your actual backend URL
+// });
+
+// axiosInstance.interceptors.request.use((config)=>{
+//   const token = store.getState().auth.token;
+//   if(token){
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// axiosInstance.interceptors.response.use(
+//   (res) => res,
+//   (err) => {
+//     if (err.response?.status === 401) {
+//       store.dispatch(logout());
+//     }
+//     return Promise.reject(err);
+//   }
+// );
+
+// export default axiosInstance;
+
+
 import store from "@/app/redux/store";
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL : "http://localhost:3001/"  // for local development
-  baseURL : "https://ecommerce-27cm.onrender.com/"  // ✅ your actual backend URL
+  baseURL : "https://backend-0iae.onrender.com/",   // 🔥 correct backend
+  // baseURL: "http://localhost:3001/"               // for local dev
 });
 
 axiosInstance.interceptors.request.use((config)=>{
